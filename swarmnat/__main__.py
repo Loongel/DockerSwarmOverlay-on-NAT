f3bdeb0200e1e02fdbae599a26f8f1a73b3b4fb7
Loongel <9842313+Loongel@users.noreply.github.com>
2023-07-26 16:35:00 +0800

    bugfix: 1.增加__main.py__的参数解析，使得run.sh可正常工作

--- swarmnat/__main__.py
+++ swarmnat/__main__.py
@@ -1,10 +1,23 @@
 """Swarm Nat Tools: help docker swarm overlay nodes work on nat networks"""
 
+import argparse
 from swarmnat.utils.nodes import Node, Nodes, NetworkType, NatNodesRelays
 from swarmnat.utils.network_manager import NetworkManager
 from swarmnat.config import read_config_file
 
+
 def main(cmd=None):
+    
+    # Initialize args
+        
+    if cmd is None:
+        parser = argparse.ArgumentParser(description='Swarmnat command-line tool.')
+        parser.add_argument('cmd', nargs='?', default='clear', type=str,
+                            choices=['nat','clear','clear_all'], help='The command to execute (e.g., \
+                                "nat":create nat rules for docker swarm; "clear":clear nat rules,"clear_all":clear all docker rules).')
+        args = parser.parse_args()
+        cmd = args.cmd
+        
     # Initialize nodes and nat_nodes_relays
     
     conf = read_config_file('config.yml')
@@ -21,11 +34,20 @@ def main(cmd=None):
 
     if cmd is None or cmd =='nat':
         # 根据通讯链路查找本机节点的nat任务，并执行
+        print("Start to handle on chains...\n")
         network_manager.handle_on_chains()
+        
     elif cmd =='clear':
+        print("Clear swarmnat iptables rules...\n")
         network_manager.clear_swarmnat_iptables_rules(mode='nat')
+        
     elif cmd =='clear_all':
+        print("Clear all docker iptables rules...\n")
         network_manager.clear_swarmnat_iptables_rules(mode='all')
+        
+    else:
+        print("Invalid command, please check the command and the terminal output")
+        return
         
 if __name__ == "__main__":
     main()
